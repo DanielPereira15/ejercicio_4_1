c02f398ee75158841d63b6555036ad8c172a0ed8
dani9 <dani9@DESKTOP-DG987T8>
2020-11-18 22:57:56 +0100

    cambio 2 en una rama nueva clase

---
+++ ejercicio_4_1/src/cambio1/Cambio2.java
@@ -0,0 +1,10 @@
+package cambio1;
+
+public class Cambio2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Cambio 2 en una rama");
+	}
+
+}
